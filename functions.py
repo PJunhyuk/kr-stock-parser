c34a7947cb88f39f4a7aa3379c512e2b0fc8f02c
PJunhyuk <jgravity@naver.com>
2018-05-03 00:58:29 +0900

    Add import pandas in functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def save_as_csv():
     code_df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13', header=0)[0]
     # print(code_df)
